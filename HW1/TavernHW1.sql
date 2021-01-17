6c18e9035542901ddcecf0a442f711f7a4f41a3e
Alwin Kuruvilla <alwin@akxlphotos.com>
2021-01-16 22:39:54 -0500

    Create initial tables from class and seeded them.

--- HW1/TavernHW1.sql
+++ HW1/TavernHW1.sql
@@ -1,129 +1,109 @@
-DROP TABLE IF EXISTS Sales
-DROP TABLE IF EXISTS Inventory
-DROP TABLE IF EXISTS Supplies
-DROP TABLE IF EXISTS BasementRats
-DROP TABLE IF EXISTS Services
-DROP TABLE IF EXISTS ReceivedGoods
-DROP TABLE IF EXISTS Status
-DROP TABLE IF EXISTS UserRoles
-DROP TABLE IF EXISTS Roles
-DROP TABLE IF EXISTS Taverns
-DROP TABLE IF EXISTS Users
-DROP TABLE IF EXISTS Locations
+-------------------
+--DROPPING TABLES--
+-------------------
+DROP TABLE IF EXISTS UserRoles;
+DROP TABLE IF EXISTS Taverns;
+DROP TABLE IF EXISTS Roles;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Locations;
 
-CREATE TABLE [Locations]
+-------------------
+--CREATING TABLES--
+-------------------
+CREATE TABLE Locations
 (
-    [LocationID]    int IDENTITY (1, 1) PRIMARY KEY ,
-    [Name]          nvarchar(50)
+    LocationID int IDENTITY (1, 1),
+    LocationName       nvarchar(50)
 );
+
+CREATE TABLE Users
+(
+    UserID       int IDENTITY (1, 1),
+    UserName     nvarchar(50),
+    UserBirthday datetime
+);
+
+CREATE TABLE Roles
+(
+    RolesID  int IDENTITY (1, 1),
+    RoleName nvarchar(50)
+);
+
+CREATE TABLE Taverns
+(
+    TavernID    int IDENTITY (1,1),
+    TavernName  nvarchar(50),
+    FloorsCount int,
+    UserID      int,
+    LocationID  int
+
+);
+
+CREATE TABLE Rats
+(
+    RatID   int IDENTITY (1,1),
+    TavernID    int,
+    RatName     nvarchar(50)
+);
+
+CREATE TABLE UserRoles
+(
+    UserID int,
+    RoleID int
+);
+
+------------------
+--SEEDING TABLES--
+------------------
 INSERT INTO Locations
-    (Name)
+    (LocationName)
 VALUES
-    ('McDonald''s')
+    ('New York City'),
+    ('Philadelphia'),
+    ('Boston'),
+    ('Quahog'),
+    ('Springfield');
 
-CREATE TABLE [Users]
-(
-    [UserID]    int IDENTITY (1, 1) PRIMARY KEY ,
-    [UserName]  nvarchar(50),
-    [UserBirthday]  datetime
-);
+INSERT INTO Users
+    (UserName, UserBirthday)
+VALUES
+    ('Alwin', 19830710),
+    ('Betsy', 19831229),
+    ('Carl', 19840214),
+    ('Danielle', 19580819),
+    ('Eric', 20030625),
+    ('Fran', 19981130),
+    ('Giovanna', 20000305),
+    ('Henry', 19600417),
+    ('Isaac', 20020329),
+    ('Jenna', 19860521);
 
-CREATE TABLE [Taverns]
-(
-    [TavernID]      int IDENTITY (1,1) PRIMARY KEY ,
-    [TavernName]    nvarchar(50),
-    [FloorsCount]   int,
-    [UserID]        int FOREIGN KEY REFERENCES [Users],
-    [LocationID]    int FOREIGN KEY REFERENCES [Locations]
+INSERT INTO Roles
+    (RoleName)
+VALUES
+    ('Owner'),
+    ('Bartender'),
+    ('Host'),
+    ('Cook'),
+    ('Server'),
+    ('Busboy');
 
-);
+INSERT INTO Taverns
+    (TavernName, FloorsCount, UserID, LocationID)
+VALUES
+    ('Moe''s', 2, 3, 5),
+    ('O''Lunney''s', 3, 1, 1),
+    ('Chickie and Pete''s', 1, 2, 2),
+    ('Drunken Clam', 1, 5, 4),
+    ('Elle''s Steak and Beer', 3, 4, 3);
 
-CREATE TABLE [Roles]
-(
-    [RolesID]   int IDENTITY (1, 1) PRIMARY KEY ,
-    [RoleName]  nvarchar(50)
-);
-
-CREATE TABLE [UserRoles]
-(
-    [UserID]    int FOREIGN KEY REFERENCES [Users],
-    [RoleID]    int FOREIGN KEY REFERENCES [Roles]
-);
-
-CREATE TABLE [Status]
-(
-    [StatusID]      int IDENTITY (1, 1) PRIMARY KEY,
-    [StatusName]    nvarchar(50),
-    ServiceID       int
-);
-
-CREATE TABLE ReceivedGoods
-(
-    [ReceivedID]    int IDENTITY (1, 1) PRIMARY KEY,
-    [TavernID]      int,
-    [SupplyID]      int,
-    [AmtReceived]   nvarchar(50),
-    [DateReceived]  datetime
-);
-
-CREATE TABLE [Services]
-(
-    [ServiceID]     int IDENTITY (1, 1) PRIMARY KEY ,
-    [ServiceName]   nvarchar(50),
-    [ServicePrice]  money
-);
-
-CREATE TABLE [BasementRats]
-(
-    [RatID]    int IDENTITY (1, 1) PRIMARY KEY ,
-    [TavernID] int,
-    [RatName]  nvarchar(50)
-);
-
-CREATE TABLE [Supplies]
-(
-    [SupplyID]      int IDENTITY (1, 1) PRIMARY KEY ,
-    [SupplyName]    nvarchar(50),
-    [SupplyUnit]    nvarchar(50),
-    [TavernID]      int FOREIGN KEY REFERENCES [Taverns]
-);
-
-CREATE TABLE [Inventory]
-(
-    [InventoryID]   int IDENTITY (1, 1) PRIMARY KEY,
-    [SupplyID]      int FOREIGN KEY REFERENCES [Supplies],
-    [DateUpdated]   datetime,
-);
+INSERT INTO Rats
+    (TavernID, RatName)
+VALUES
+    (3, 'Jim'),
+    (3, 'Tom'),
+    (1, 'Pete'),
+    (1, 'Chester'),
+    (2, 'Phil');
 
 
-CREATE TABLE [Sales]
-(
-    [SalesID]       int IDENTITY (1, 1) PRIMARY KEY ,
-    [UserID]        int FOREIGN KEY REFERENCES [Users],
-    [TavernID]      int FOREIGN KEY REFERENCES [Taverns],
-    [ServiceID]     int FOREIGN KEY REFERENCES [Services],
-    [DatePurchased] datetime
-)
-
-CREATE TABLE [Classes]
-(
-    [Class] nvarchar(50) UNIQUE
-)
-
-CREATE TABLE [Guests]
-(
-    [GuestID]   int IDENTITY (1, 1) PRIMARY KEY,
-    [GuestName] nvarchar(50),
-    [GuestBirthday] datetime,
-    [GuestCakeday]  datetime,
-    [GuestStatus]   nvarchar(50),
-)
-
-CREATE TABLE [GuestClass]
-(
-    [GuestClassID] int IDENTITY (1, 1),
-    [Class] int FOREIGN KEY REFERENCES [Classes](Class),
-    [Level] int
-)
-
-DROP TABLE IF EXISTS [BasementRats]
